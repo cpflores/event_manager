ee9874d067744d3f7acf16345a5ec73e53738760
Chadwick Flores <chadwickflores@gmail.com>
2014-05-11 12:01:00 -0500

    add form letter and erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,14 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
+
+meaning_of_life = 42
+
+question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
+template = ERB.new question
+
+results = template.result(binding)
+puts results
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -14,17 +23,22 @@ def legislators_by_zipcode(zipcode)
   	"#{legislator.first_name} #{legislator.last_name}"
 	end
 
-	legislator_names.join(", ")
-	
+	legislator_names.join(", ")	
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
+template_letter = File.read "form_letter.html"
+
 contents.each do |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
 	legislators = legislators_by_zipcode(zipcode)
-  puts "#{name} #{zipcode} #{legislators}"
+  
+  personal_letter = template_letter.gsub('FIRST_NAME',name)
+  personal_letter.gsub!('LEGISLATORS',legislators)
+
+  puts personal_letter
 end
