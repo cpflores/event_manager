8a56c45bf4c8d8194633b2a327c6d5ada0e8db64
Chadwick Flores <chadwickflores@gmail.com>
2014-05-11 11:53:07 -0500

    iteration 3 complete, legislators call refactored

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,6 +7,17 @@ def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5,"0")[0..4]
 end 
 
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.collect do |legislator|
+  	"#{legislator.first_name} #{legislator.last_name}"
+	end
+
+	legislator_names.join(", ")
+	
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
@@ -14,13 +25,6 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
-	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-	legislator_names = legislators.collect do |legislator|
-  	"#{legislator.first_name} #{legislator.last_name}"
-	end
-
-	legislators_string = legislator_names.join(", ")
-
-  puts "#{name} #{zipcode} #{legislators_string}"
+	legislators = legislators_by_zipcode(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
 end
