18298b50dcd35ec365f64688b5be806b03e3b627
Chadwick Flores <chadwickflores@gmail.com>
2014-05-10 19:21:49 -0500

    cleaned code to return names and zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,23 @@
+require "csv"
+
+def clean_zipcode(zipcode)
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif zipcode.length < 5
+		zipcode = zipcode.rjust 5, "0"
+	elsif zipcode.length > 5
+		zipcode = zipcode[0..4]
+	end
+end 
+
 puts "EventManager initialized."
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line,index|
-	next if index == 0
-	columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+	name = row[:first_name]
+	
+	zipcode = row[:zipcode]
+
+  puts "#{name} #{zipcode}"
 end
