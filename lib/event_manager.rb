e25499390392c5b6aeee604035500136f81a1ad0
Chadwick Flores <chadwickflores@gmail.com>
2014-05-12 09:34:21 -0500

    iteration 4 complete, created customized letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,14 +2,6 @@ require 'csv'
 require 'sunlight/congress'
 require 'erb'
 
-meaning_of_life = 42
-
-question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
-template = ERB.new question
-
-results = template.result(binding)
-puts results
-
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
@@ -17,28 +9,33 @@ def clean_zipcode(zipcode)
 end 
 
 def legislators_by_zipcode(zipcode)
-	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+	Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-	legislator_names = legislators.collect do |legislator|
-  	"#{legislator.first_name} #{legislator.last_name}"
-	end
-
-	legislator_names.join(", ")	
+def save_thank_you_letters(id,form_letter)
+	Dir.mkdir("output") unless Dir.exists? "output"
+  
+  filename = "output/thanks_#{id}.html"
+  
+  File.open(filename,'w') do |file|
+  	file.puts form_letter
+  end
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.html"
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+	id = row[0]
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
 	legislators = legislators_by_zipcode(zipcode)
   
-  personal_letter = template_letter.gsub('FIRST_NAME',name)
-  personal_letter.gsub!('LEGISLATORS',legislators)
-
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+  
+  save_thank_you_letters(id,form_letter)
 end
